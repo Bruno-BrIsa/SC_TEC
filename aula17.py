653fecfee61a4a7eb2e3bc8060758e902c78e1f8
Bruno <brunobp776@gmail.com>
2026-02-22 20:06:39 -0300

    aula17.py removida

--- aula17.py
+++
@@ -1,6 +0,0 @@
-# Código para mostrar valores ordenados da coluna de temperatura
-import pandas as pd
-
-df = pd.read_csv('Ice Cream Sales - temperatures.csv')
-temperatures = df['Temperature'].sort_values()
-print(temperatures)
